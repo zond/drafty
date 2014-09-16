4ecfd6a1f06511a4ac6334679ad2f0877065522c
Martin Bruse <zondolfin@gmail.com>
2014-09-16 19:03:34 +0200

    made the merkle trees work on alphabetically sorted data instead of numerically sorted.

--- storage/storage.go
+++ storage/storage.go
@@ -11,7 +11,6 @@ import (
 
 var valueBucketKey = []byte("values")
 var merkleBucketKey = []byte("merkles")
-var maxLevelKey = []byte("maxLevel")
 
 type DB interface {
 	Hash() ([]byte, error)
@@ -20,6 +19,9 @@ type DB interface {
 	Put([]byte, []byte) error
 	PutString(string, string) error
 	Get([]byte) ([]byte, error)
+	GetString(string) (string, error)
+	Delete([]byte) error
+	DeleteString(string) error
 }
 
 type db struct {
@@ -49,20 +51,9 @@ func (self *db) concat(h1 uint64, h2 uint64) []byte {
 	}
 }
 
-func merkleLevelKey(l int) (result []byte) {
+func merkleLevelKey(l uint) (result []byte) {
 	result = make([]byte, binary.MaxVarintLen64)
 	result = result[:binary.PutUvarint(result, uint64(l))]
-	return
-}
-
-func levelStart(level int, key []byte) (result []byte) {
-	result = make([]byte, len(key))
-	copy(result, key)
-	mask := byte(0xf0)
-	for i := 0; i < level && i < len(key)*2; i++ {
-		result[len(result)-1-i/2] &= mask
-		mask = 0xff ^ mask
-	}
 	return
 }
 
@@ -74,34 +65,43 @@ func fmtBytes(b []byte) string {
 	return buf.String()
 }
 
-func levelEnd(level int, key []byte) (result []byte) {
-	result = levelStart(level, key)
-	if len(result) < 1+level/2 {
-		newRes := make([]byte, 1+level/2)
-		copy(newRes, result)
-		result = newRes
+func levelStart(level uint, key []byte) (result []byte) {
+	if level == 0 {
+		return []byte{0}
 	}
-	pos := len(result) - 1 - level/2
-	result[pos] += 1 << (4 * (uint(level) % 2))
+	if level > uint(len(key)) {
+		level = uint(len(key))
+	}
+	return key[:level]
+}
+
+func levelEnd(level uint, key []byte) (result []byte) {
+	if level == 0 {
+		return []byte{0}
+	}
+	start := levelStart(level, key)
+	result = make([]byte, len(start))
+	copy(result, start)
+	pos := len(result) - 1
+	result[pos] = result[pos] + 1
 	for result[pos] == 0 {
 		if pos == 0 {
-			newRes := make([]byte, len(result)+1)
-			copy(newRes[1:], result)
-			result = newRes
-		} else {
-			pos--
+			result = []byte{0}
+			break
 		}
-		result[pos] += 1
+		pos--
+		result[pos]++
 	}
 	return
 }
 
-func (self *db) hash(bucket *bolt.Bucket, key []byte, level int) (result []byte, start []byte, sources int, err error) {
+func (self *db) hash(bucket *bolt.Bucket, key []byte, level uint) (result []byte, start []byte, sources int, err error) {
 	values := bucket.Cursor()
 	start = levelStart(level, key)
 	end := levelEnd(level, key)
+	toTheEnd := len(end) == 0 && end[0] == 0
 	hash := murmur3.New128()
-	for k, v := values.Seek(start); k != nil && bytes.Compare(k, end) < 0; k, v = values.Next() {
+	for k, v := values.Seek(start); k != nil && (toTheEnd || bytes.Compare(k, end) < 0); k, v = values.Next() {
 		if _, err = hash.Write(v); err != nil {
 			return
 		}
@@ -111,59 +111,59 @@ func (self *db) hash(bucket *bolt.Bucket, key []byte, level int) (result []byte,
 	return
 }
 
-func (self *db) getUint(bucket *bolt.Bucket, key []byte) (result int) {
-	i, _ := binary.Varint(bucket.Get(key))
-	result = int(i)
+func (self *db) getUint(bucket *bolt.Bucket, key []byte) (result uint) {
+	k := bucket.Get(key)
+	if k == nil {
+		return 0
+	}
+	i, _ := binary.Uvarint(k)
+	result = uint(i)
 	return
 }
 
-func (self *db) putUint(bucket *bolt.Bucket, key []byte, i int) (err error) {
+func (self *db) putUint(bucket *bolt.Bucket, key []byte, i uint) (err error) {
 	b := make([]byte, binary.MaxVarintLen64)
-	b = b[:binary.PutVarint(b, int64(i))]
+	b = b[:binary.PutUvarint(b, uint64(i))]
 	return bucket.Put(key, b)
 }
 
 func (self *db) updateMerkle(valueBucket *bolt.Bucket, merkleBucket *bolt.Bucket, key []byte) (err error) {
-	level := 1
-	maxLevel := self.getUint(merkleBucket, maxLevelKey)
-	if level > maxLevel {
-		if err = self.putUint(merkleBucket, maxLevelKey, level); err != nil {
-			return
-		}
+	level := uint(len(key))
+	merkles, err := merkleBucket.CreateBucketIfNotExists(merkleLevelKey(level))
+	if err != nil {
+		return
 	}
 	hash, levelStart, sources, err := self.hash(valueBucket, key, level)
 	if err != nil {
 		return
 	}
-	merkles, err := merkleBucket.CreateBucketIfNotExists(merkleLevelKey(level))
-	if err != nil {
-		return
-	}
-	if err = merkles.Put(levelStart, hash); err != nil {
-		return
-	}
-	level++
-	valueBucket = merkles
-	for {
-		if hash, levelStart, sources, err = self.hash(valueBucket, key, level); err != nil {
-			return
-		}
-		if sources == 1 {
-			break
-		}
-		if level > maxLevel {
-			if err = self.putUint(merkleBucket, maxLevelKey, level); err != nil {
-				return
-			}
-		}
-		if merkles, err = merkleBucket.CreateBucketIfNotExists(merkleLevelKey(level)); err != nil {
-			return
-		}
+	if sources > 0 {
 		if err = merkles.Put(levelStart, hash); err != nil {
 			return
 		}
-		level++
+	} else {
+		if err = merkles.Delete(levelStart); err != nil {
+			return
+		}
+	}
+	for level > 0 {
 		valueBucket = merkles
+		level--
+		if merkles, err = merkleBucket.CreateBucketIfNotExists(merkleLevelKey(level)); err != nil {
+			return
+		}
+		if hash, levelStart, sources, err = self.hash(valueBucket, key, level); err != nil {
+			return
+		}
+		if sources > 0 {
+			if err = merkles.Put(levelStart, hash); err != nil {
+				return
+			}
+		} else {
+			if err = merkles.Delete(levelStart); err != nil {
+				return
+			}
+		}
 	}
 	return
 }
@@ -187,8 +187,7 @@ func (self *db) Hash() (result []byte, err error) {
 		if err != nil {
 			return
 		}
-		maxLevel := self.getUint(merkleBucket, maxLevelKey)
-		_, result = merkleBucket.Bucket(merkleLevelKey(maxLevel)).Cursor().First()
+		_, result = merkleBucket.Bucket(merkleLevelKey(0)).Cursor().First()
 		return
 	}); err != nil {
 		return
@@ -198,6 +197,15 @@ func (self *db) Hash() (result []byte, err error) {
 
 func (self *db) PutString(key string, value string) (err error) {
 	return self.Put([]byte(key), []byte(value))
+}
+
+func (self *db) GetString(key string) (result string, err error) {
+	res, err := self.Get([]byte(key))
+	if err != nil {
+		return
+	}
+	result = string(res)
+	return
 }
 
 func (self *db) Get(key []byte) (result []byte, err error) {
@@ -214,6 +222,32 @@ func (self *db) Get(key []byte) (result []byte, err error) {
 	return
 }
 
+func (self *db) DeleteString(key string) (err error) {
+	return self.Delete([]byte(key))
+}
+
+func (self *db) Delete(key []byte) (err error) {
+	if err = self.bolt.Update(func(tx *bolt.Tx) (err error) {
+		valueBucket, err := tx.CreateBucketIfNotExists(valueBucketKey)
+		if err != nil {
+			return
+		}
+		if err = valueBucket.Delete(key); err != nil {
+			return
+		}
+		merkleBucket, err := tx.CreateBucketIfNotExists(merkleBucketKey)
+		if err != nil {
+			return
+		}
+		if err = self.updateMerkle(valueBucket, merkleBucket, key); err != nil {
+			return
+		}
+		return
+	}); err != nil {
+		return
+	}
+	return
+}
 func (self *db) Put(key []byte, value []byte) (err error) {
 	if err = self.bolt.Update(func(tx *bolt.Tx) (err error) {
 		valueBucket, err := tx.CreateBucketIfNotExists(valueBucketKey)
--- storage/storage_test.go
+++ storage/storage_test.go
@@ -1,13 +1,18 @@
 package storage
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
-	"math/big"
 	"math/rand"
 	"os"
 	"testing"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
 
 func withDB(t *testing.T, f func(DB)) {
 	fname := fmt.Sprintf("test-%v.db", rand.Int())
@@ -33,30 +38,30 @@ func TestLevels(t *testing.T) {
 		for index, _ := range key {
 			key[index] = byte(rand.Int())
 		}
-		keyBn := big.NewInt(0).SetBytes(key)
 		for lvl := 1; lvl < 24; lvl++ {
-			start := levelStart(lvl, key)
-			startBn := big.NewInt(0).SetBytes(start)
-			end := levelEnd(lvl, key)
-			endBn := big.NewInt(0).SetBytes(end)
-			if startBn.Cmp(keyBn) > 0 {
+			start := levelStart(uint(lvl), key)
+			end := levelEnd(uint(lvl), key)
+			if bytes.Compare(start, key) > 0 {
 				t.Errorf("Start level of %v for %v is %v, but that is greater than the key", lvl, fmtBytes(key), fmtBytes(start))
 			}
-			if endBn.Cmp(keyBn) < 1 {
+			if (len(end) != 1 || end[0] != 0) && bytes.Compare(end, key) < 1 {
 				t.Errorf("End of level %v for %v is %v, but that is not greater than the key", lvl, fmtBytes(key), fmtBytes(end))
-			}
-			if big.NewInt(0).Sub(endBn, startBn).Cmp(big.NewInt(0).Lsh(big.NewInt(1), uint(lvl*4))) != 0 {
-				t.Errorf("End of level %v for %v is %v, and start of level %v is %v, but they are not at the right distance", lvl, fmtBytes(key), fmtBytes(end), lvl, fmtBytes(start))
 			}
 		}
 	}
 }
+
 func TestTopHash(t *testing.T) {
 	keys := [][]byte{}
 	values := [][]byte{}
+	toDelete := [][]byte{}
 	for i := 0; i < 100; i++ {
 		key := make([]byte, binary.MaxVarintLen64)
-		keys = append(keys, key[:binary.PutVarint(key, rand.Int63())])
+		key = key[:binary.PutVarint(key, rand.Int63())]
+		keys = append(keys, key)
+		if rand.Int()%2 == 0 {
+			toDelete = append(toDelete, key)
+		}
 		value := make([]byte, binary.MaxVarintLen64)
 		values = append(values, value[:binary.PutVarint(value, rand.Int63())])
 	}
@@ -67,8 +72,18 @@ func TestTopHash(t *testing.T) {
 					t.Fatalf("%v", err)
 				}
 			}
+			for _, i := range rand.Perm(len(toDelete)) {
+				if err := db1.Delete(toDelete[i]); err != nil {
+					t.Fatalf("%v", err)
+				}
+			}
 			for _, i := range rand.Perm(len(keys)) {
 				if err := db2.Put(keys[i], values[i]); err != nil {
+					t.Fatalf("%v", err)
+				}
+			}
+			for _, i := range rand.Perm(len(toDelete)) {
+				if err := db2.Delete(toDelete[i]); err != nil {
 					t.Fatalf("%v", err)
 				}
 			}
