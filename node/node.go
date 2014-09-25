7a893e2ba3dc33b913b079645e6bd02bd23ac9ec
Martin Bruse <zondolfin@gmail.com>
2014-09-25 14:13:43 +0200

    removed the redundant nameKey. registered the Add/RemovePeerCommands. extracted the Ring type from node.go. gave nodes separat pos and name (pos is position in ring, name is permanent raft name) which allows me to use completly random position and still have json safe names. made the stopping happen with a sync.Cond again, since the waitgroup doesnt lock the way i want. made nodes save the ring state as well as whether they are stopped. made sync actually sync data. moved some packages around.

--- node/node.go
+++ node/node.go
@@ -1,13 +1,12 @@
 package node
 
 import (
-	"encoding/hex"
-	"encoding/json"
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"math/rand"
 	"os"
 	"path/filepath"
-	"sort"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -15,27 +14,33 @@ import (
 	"github.com/goraft/raft"
 	"github.com/zond/drafty/log"
 	"github.com/zond/drafty/node/commands"
-	"github.com/zond/drafty/raft/transport"
+	"github.com/zond/drafty/node/ring"
+	raftTransport "github.com/zond/drafty/raft/transport"
 	"github.com/zond/drafty/storage"
+	storageTransport "github.com/zond/drafty/storage/transport"
 	"github.com/zond/drafty/switchboard"
 )
 
 var metadataBucketKey = []byte("metadata")
-var nameKey = []byte("name")
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 	raft.RegisterCommand(&commands.StopCommand{})
 	raft.RegisterCommand(&commands.ContCommand{})
+	raft.RegisterCommand(&commands.AddPeerCommand{})
+	raft.RegisterCommand(&commands.RemovePeerCommand{})
 }
 
 type Node struct {
+	pos            []byte
 	server         *switchboard.Server
 	dir            string
 	raft           raft.Server
+	ring           *ring.Ring
 	stopped        int32
 	stops          uint64
-	stoppedGroup   sync.WaitGroup
+	stopLock       sync.Mutex
+	stopCond       *sync.Cond
 	procsDoneGroup sync.WaitGroup
 	storage        storage.DB
 }
@@ -44,7 +49,9 @@ func New(addr string, dir string) (result *Node, err error) {
 	result = &Node{
 		server: switchboard.NewServer(addr),
 		dir:    dir,
+		ring:   ring.New(),
 	}
+	result.stopCond = sync.NewCond(&result.stopLock)
 	if err = os.MkdirAll(result.dir, 0700); err != nil {
 		return
 	}
@@ -60,95 +67,50 @@ func (self MultiError) Error() string {
 	return fmt.Sprint([]error(self))
 }
 
+type nodeState struct {
+	Ring    *ring.Ring
+	Stopped int32
+}
+
 func (self *Node) Save() (b []byte, err error) {
 	log.Debugf("Compacting log")
+	buf := &bytes.Buffer{}
+	if err = gob.NewEncoder(buf).Encode(nodeState{
+		Ring:    self.ring,
+		Stopped: atomic.LoadInt32(&self.stopped),
+	}); err != nil {
+		return
+	}
 	return
 }
 
 func (self *Node) Recovery(b []byte) (err error) {
 	log.Debugf("Recovering from log")
+	state := &nodeState{}
+	if err = gob.NewDecoder(bytes.NewBuffer(b)).Decode(state); err != nil {
+		return
+	}
+	self.ring = state.Ring
+	atomic.StoreInt32(&self.stopped, state.Stopped)
 	return
 }
 
 func (self *Node) Stop() (err error) {
-	self.stoppedGroup.Add(1)
+	self.stopLock.Lock()
+	defer self.stopLock.Unlock()
+	atomic.StoreInt32(&self.stopped, 1)
 	atomic.AddUint64(&self.stops, 1)
 	self.procsDoneGroup.Wait()
+	log.Debugf("%v stopped", self)
 	return
 }
 
 func (self *Node) Continue() (err error) {
-	self.stoppedGroup.Done()
+	atomic.StoreInt32(&self.stopped, 0)
+	self.stopCond.Broadcast()
 	go self.synchronize()
+	log.Debugf("%v continues", self)
 	return
-}
-
-type ring []*raft.Peer
-
-func (self ring) Len() int {
-	return len(self)
-}
-
-func (self ring) Less(i, j int) bool {
-	return self[i].Name < self[j].Name
-}
-
-func (self ring) Swap(i, j int) {
-	self[i], self[j] = self[j], self[i]
-}
-
-func (self ring) successors(name string, n int) (result ring) {
-	result = make(ring, 0, n)
-	for i := sort.Search(len(self), func(p int) bool { return self[p].Name > name }); len(result) < n; i++ {
-		result = append(result, self[i%len(self)])
-	}
-	return
-}
-
-func (self *Node) ring() (result ring) {
-	peers := self.raft.Peers()
-	result = make(ring, 0, 1+len(peers))
-	result = append(result, &raft.Peer{
-		Name:             self.raft.Name(),
-		ConnectionString: self.server.Addr(),
-	})
-	for _, peer := range peers {
-		result = append(result, peer)
-	}
-	sort.Sort(result)
-	return
-}
-
-func (self *Node) successor() (result *raft.Peer) {
-	return self.ring().successors(self.raft.Name(), 1)[0]
-}
-
-func (self *Node) responsibility() (result storage.Range) {
-	result.FromInc = []byte(self.raft.Name())
-	result.ToExc = []byte(self.successor().Name)
-	return
-}
-
-func (self *Node) sync(peer *raft.Peer, r storage.Range) (acted bool, err error) {
-	log.Debugf("%v syncing %v with %v\n", self.name(), r, hex.EncodeToString([]byte(peer.Name)))
-	return
-}
-
-func (self *Node) backups() (result ring) {
-	return self.ring().successors(self.raft.Name(), 3)
-}
-
-func (self *Node) syncBackups() {
-	r := self.responsibility()
-	stops := atomic.LoadUint64(&self.stops)
-	for _, backup := range self.backups() {
-		for acted, err := self.sync(backup, r); err == nil && acted && atomic.LoadUint64(&self.stops) == stops; acted, err = self.sync(backup, r) {
-		}
-	}
-}
-
-func (self *Node) synchronize() {
-	self.syncBackups()
 }
 
 func (self *Node) WhileStopped(f func() error) (err error) {
@@ -170,9 +132,76 @@ func (self *Node) WhileStopped(f func() error) (err error) {
 	return
 }
 
-func (self *Node) randomName() (result string) {
+func (self *Node) WhileRunning(f func() error) error {
+	self.stopLock.Lock()
+	for atomic.LoadInt32(&self.stopped) == 1 {
+		self.stopCond.Wait()
+	}
+	self.stopLock.Unlock()
+	self.procsDoneGroup.Add(1)
+	defer self.procsDoneGroup.Done()
+	return f()
+}
+
+func (self *Node) AddPeer(peer *ring.Peer) (err error) {
+	self.ring.AddPeer(peer)
+	return
+}
+
+func (self *Node) RemovePeer(name string) (err error) {
+	self.ring.RemovePeer(name)
+	return
+}
+
+func (self *Node) successor() (result *ring.Peer) {
+	return self.ring.Successors(self.pos, 1)[0]
+}
+
+func (self *Node) responsibility() (result storage.Range) {
+	result.FromInc = self.pos
+	result.ToExc = []byte(self.successor().Pos)
+	return
+}
+
+func (self *Node) sync(peer *ring.Peer, r storage.Range) (acted bool, err error) {
+	if err = self.WhileRunning(func() (err error) {
+		ops, err := self.storage.Sync(storageTransport.RPCTransport(peer.ConnectionString), r, 1)
+		if err != nil {
+			return
+		}
+		acted = ops > 0
+		log.Debugf("%v synced %v values within %v with %v\n", self, ops, r, peer)
+		return
+	}); err != nil {
+		return
+	}
+	return
+}
+
+func (self *Node) String() string {
+	return self.AsPeer().String()
+}
+
+func (self *Node) backups() (result ring.Peers) {
+	return self.ring.Successors(self.pos, 3)
+}
+
+func (self *Node) syncBackups() {
+	r := self.responsibility()
+	stops := atomic.LoadUint64(&self.stops)
+	for _, backup := range self.backups() {
+		for acted, err := self.sync(backup, r); err == nil && acted && atomic.LoadUint64(&self.stops) == stops; acted, err = self.sync(backup, r) {
+		}
+	}
+}
+
+func (self *Node) synchronize() {
+	self.syncBackups()
+}
+
+func (self *Node) randomPos() (result []byte) {
 	i := rand.Int63()
-	b, err := json.Marshal(string([]byte{
+	result = []byte{
 		byte(i >> 7),
 		byte(i >> 6),
 		byte(i >> 5),
@@ -181,18 +210,16 @@ func (self *Node) randomName() (result string) {
 		byte(i >> 2),
 		byte(i >> 1),
 		byte(i >> 0),
-	}))
-	if err != nil {
-		return
-	}
-	if err = json.Unmarshal(b, &result); err != nil {
-		return
 	}
 	return
 }
 
-func (self *Node) name() string {
-	return hex.EncodeToString([]byte(self.raft.Name()))
+func (self *Node) AsPeer() (result *ring.Peer) {
+	return &ring.Peer{
+		Name:             self.raft.Name(),
+		Pos:              self.pos,
+		ConnectionString: self.server.Addr(),
+	}
 }
 
 func (self *Node) Start(join string) (err error) {
@@ -200,7 +227,7 @@ func (self *Node) Start(join string) (err error) {
 		err = fmt.Errorf("Node is already started")
 		return
 	}
-	name := self.randomName()
+	self.pos = self.randomPos()
 	logdir := filepath.Join(self.dir, "raft.log")
 	if err = os.RemoveAll(logdir); err != nil {
 		return
@@ -208,41 +235,51 @@ func (self *Node) Start(join string) (err error) {
 	if err = os.MkdirAll(logdir, 0700); err != nil {
 		return
 	}
-	rpcTransport := &transport.RPCTransport{
+	rpcTransport := &raftTransport.RPCTransport{
 		Stopper: self,
 	}
-	if self.raft, err = raft.NewServer(name, logdir, rpcTransport, self, self, self.server.Addr()); err != nil {
+	if self.raft, err = raft.NewServer(fmt.Sprintf("%08x", rand.Int63()), logdir, rpcTransport, self, self, self.server.Addr()); err != nil {
 		return
 	}
 	rpcTransport.Raft = self.raft
-	self.server.Serve("Raft", &transport.RPC{
-		Stopper: self,
-		Raft:    self.raft,
+	self.server.Serve("Raft", &raftTransport.RPCServer{
+		Raft: self.raft,
 	})
-	self.server.Serve("Synchronizable", &storage.RPC{
+	self.server.Serve("Synchronizable", &storageTransport.RPCServer{
 		Storage: self.storage,
+	})
+	self.server.Serve("Node", &RPCServer{
+		node: self,
 	})
 	if err = self.raft.Start(); err != nil {
 		return
 	}
 	if join == "" {
-		cmd := &raft.DefaultJoinCommand{
+		if _, err = self.raft.Do(&raft.DefaultJoinCommand{
 			Name:             self.raft.Name(),
 			ConnectionString: self.server.Addr(),
-		}
-		if _, err = self.raft.Do(cmd); err != nil {
+		}); err != nil {
 			return
 		}
-		log.Infof("%v is cluster leader", self.name())
+		if _, err = self.raft.Do(&commands.AddPeerCommand{
+			Peer: self.AsPeer(),
+		}); err != nil {
+			return
+		}
+		log.Infof("%v is cluster leader", self)
 	} else {
-		resp := &transport.JoinResponse{}
+		joinResp := &raftTransport.JoinResponse{}
 		if err = switchboard.Switch.Call(join, "Raft.Join", &raft.DefaultJoinCommand{
 			Name:             self.raft.Name(),
 			ConnectionString: self.server.Addr(),
-		}, resp); err != nil {
+		}, joinResp); err != nil {
 			return
 		}
-		log.Infof("%v joined %#v", self.name(), hex.EncodeToString([]byte(resp.Name)))
+		master := &ring.Peer{}
+		if err = switchboard.Switch.Call(join, "Node.AddPeer", self.AsPeer(), master); err != nil {
+			return
+		}
+		log.Infof("%v joined %v", self, master)
 		return
 	}
 	return
