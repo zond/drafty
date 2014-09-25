47d992f6ae2e38469f51edd5ab3e4d1fb0f4be5c
Martin Bruse <zondolfin@gmail.com>
2014-09-25 14:09:38 +0200

    added AddPeer and RemovePeer commands that mutate the ring state.

--- node/commands/commands.go
+++ node/commands/commands.go
@@ -1,30 +1,68 @@
 package commands
 
-import "github.com/goraft/raft"
+import (
+	"github.com/goraft/raft"
+	"github.com/zond/drafty/node/ring"
+)
 
-type StopContinuer interface {
+type Stopper interface {
 	Stop() error
+}
+
+type Continuer interface {
 	Continue() error
+}
+
+type PeerAdder interface {
+	AddPeer(*ring.Peer) error
+}
+
+type PeerRemover interface {
+	RemovePeer(string) error
 }
 
 type StopCommand struct {
 }
 
 func (self *StopCommand) CommandName() string {
-	return "consensual:stop"
+	return "node/commands/StopCommand"
 }
 
 func (self *StopCommand) Apply(c raft.Context) (result interface{}, err error) {
-	return nil, c.Server().Context().(StopContinuer).Stop()
+	return nil, c.Server().Context().(Stopper).Stop()
 }
 
 type ContCommand struct {
 }
 
 func (self *ContCommand) CommandName() string {
-	return "consensual:cont"
+	return "node/commands/ContCommand"
 }
 
 func (self *ContCommand) Apply(c raft.Context) (result interface{}, err error) {
-	return nil, c.Server().Context().(StopContinuer).Continue()
+	return nil, c.Server().Context().(Continuer).Continue()
+}
+
+type AddPeerCommand struct {
+	Peer *ring.Peer
+}
+
+func (self *AddPeerCommand) CommandName() string {
+	return "node/commands/AddPeerCommand"
+}
+
+func (self *AddPeerCommand) Apply(c raft.Context) (result interface{}, err error) {
+	return nil, c.Server().Context().(PeerAdder).AddPeer(self.Peer)
+}
+
+type RemovePeerCommand struct {
+	Name string
+}
+
+func (self *RemovePeerCommand) CommandName() string {
+	return "node/commands/RemovePeerCommand"
+}
+
+func (self *RemovePeerCommand) Apply(c raft.Context) (result interface{}, err error) {
+	return nil, c.Server().Context().(PeerRemover).RemovePeer(self.Name)
 }
