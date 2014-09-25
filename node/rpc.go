2f6cc60f2e344128d6ac8c5d3290cafb4c953f73
Martin Bruse <zondolfin@gmail.com>
2014-09-25 14:14:54 +0200

    created an rpc server type for node

---
+++ node/rpc.go
@@ -0,0 +1,33 @@
+package node
+
+import (
+	"github.com/zond/drafty/log"
+	"github.com/zond/drafty/node/commands"
+	"github.com/zond/drafty/node/ring"
+	"github.com/zond/drafty/switchboard"
+)
+
+type RPCServer struct {
+	node *Node
+}
+
+func (self *RPCServer) AddPeer(peer *ring.Peer, resp *ring.Peer) (err error) {
+	if self.node.raft.Name() != self.node.raft.Leader() {
+		return switchboard.Switch.Call(self.node.raft.Peers()[self.node.raft.Leader()].ConnectionString, "Node.AddPeer", peer, resp)
+	}
+	if err = self.node.WhileStopped(func() (err error) {
+		if _, err = self.node.raft.Do(&commands.AddPeerCommand{
+			Peer: peer,
+		}); err != nil {
+			log.Warnf("Unable to add new node %v: %v", peer, err)
+			return
+		}
+		log.Infof("%v accepted %v", self.node, peer)
+		return
+	}); err != nil {
+		return
+	}
+	r := self.node.AsPeer()
+	*resp = *r
+	return
+}
